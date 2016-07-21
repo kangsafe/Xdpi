1a4a45735271294d80b1db710709e5fb231b7290
kangsafe <kangsafe@hotmail.com>
2016-07-21 23:59:12 +0800

    添加项目文件。

---
+++ XDPI/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace XDPI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.tbfname = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbpname = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "选择文件";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(94, 10);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(248, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(368, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "浏览";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(37, 46);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(253, 191);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(347, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(349, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(37, 271);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(278, 126);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "ldpi 120dpi 0.75\nmdpi 160dpi 1\nhdpi 240dpi 1.5\nxhdpi 320dpi 2\nxxhdpi 480dpi 3\nxxx" +
+    "hdpi 640dpi 4";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(421, 354);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tbfname
+            // 
+            this.tbfname.Location = new System.Drawing.Point(420, 271);
+            this.tbfname.Name = "tbfname";
+            this.tbfname.Size = new System.Drawing.Size(116, 21);
+            this.tbfname.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(366, 274);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "文件名";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(338, 317);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 12);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "资源目录前缀";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // tbpname
+            // 
+            this.tbpname.Location = new System.Drawing.Point(421, 308);
+            this.tbpname.Name = "tbpname";
+            this.tbpname.Size = new System.Drawing.Size(116, 21);
+            this.tbpname.TabIndex = 10;
+            this.tbpname.Text = "mipmap";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(94, 182);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(335, 23);
+            this.progressBar1.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(561, 409);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tbpname);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbfname);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox tbfname;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbpname;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ XDPI/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace XDPI
+{
+    public partial class Form1 : Form
+    {
+        private string savepath = "";
+        private string filefullname = "";
+        private string filename = "";
+        private string prepath = "";
+        private string ext = "";
+        public Form1()
+        {
+            InitializeComponent();
+
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = 100;
+            progressBar1.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = openFileDialog1.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                filefullname = openFileDialog1.FileName;
+                ext = Path.GetExtension(filefullname);
+                filename = Path.GetFileNameWithoutExtension(filefullname);
+                textBox1.Text = filefullname;
+                tbfname.Text = filename;
+                Image img = Image.FromFile(filefullname);
+                pictureBox1.Image = img;
+                label2.Text = "宽度:" + img.Width + "高度:" + img.Height;
+                label3.Text = "水平分辨率：" + img.HorizontalResolution + "垂直分辨率" + img.VerticalResolution;
+            }
+        }
+        private string[] paths = { "-xxxhdpi", "-xxhdpi", "-xhdpi", "-hdpi", "-mdpi", "-ldpi" };
+        private double[] rates = { 4, 3, 2, 1.5, 1, 0.75 };
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                progressBar1.Value = 0;
+                progressBar1.Show();
+                //保存路径
+                savepath = folderBrowserDialog1.SelectedPath;
+                filename = tbfname.Text + ext;
+                prepath = tbpname.Text;
+
+                for (int i = 0; i < paths.Length; i++)
+                {
+                    Thread.Sleep(1000);
+                    progressBar1.Value = i / paths.Length * 100;
+                    Bitmap oldbmp = new Bitmap(filefullname);
+                    string temp = savepath + "\\" + prepath + paths[i];
+                    if (!Directory.Exists(temp))
+                    {
+                        Directory.CreateDirectory(temp);
+                    }
+                    string fp = temp + "\\" + filename;
+                    if (!File.Exists(fp))
+                    {
+                        Bitmap bmp = GetThumbnail(oldbmp, rates[i] / rates[0]);
+                        bmp.Save(fp);
+                        bmp.Dispose();
+                    }
+                }
+            }
+            progressBar1.Hide();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        public static Bitmap GetThumbnail(Image img, double rate)
+        {
+            // 按比例缩放
+            int sWidth = img.Width;
+            int sHeight = img.Height;
+            double sW = sWidth * rate;
+            double sH = sHeight * rate;
+            Bitmap outBmp = new Bitmap((int)sW, (int)sH);
+            Graphics g = Graphics.FromImage(outBmp);
+            g.Clear(Color.Transparent);
+            // 设置画布的描绘质量         
+            g.CompositingQuality = CompositingQuality.HighQuality;
+            g.SmoothingMode = SmoothingMode.HighQuality;
+            g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            g.DrawImage(img, new Rectangle(0, 0, (int)sW, (int)sH), 0, 0, img.Width, img.Height, GraphicsUnit.Pixel);
+            g.Dispose();
+            // 以下代码为保存图片时，设置压缩质量     
+            EncoderParameters encoderParams = new EncoderParameters();
+            long[] quality = new long[1];
+            quality[0] = 100;
+            EncoderParameter encoderParam = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, quality);
+            encoderParams.Param[0] = encoderParam;
+            img.Dispose();
+            return outBmp;
+        }
+    }
+}
